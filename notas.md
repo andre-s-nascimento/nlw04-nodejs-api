f5ad6d56b927454b92fa35f54f29a158910c86d1
André Soares Nascimento <andresoaresnascimento@gmail.com>
2021-02-23 11:33:56 -0300

    Pausa aos 45:42 video - router funcionando

--- notas.md
+++ notas.md
@@ -1,4 +1,11 @@
 ## Migrations dia 2 NLW04
 
 ### Migration de usuários
-Histórico de alterações do DB
+
+- Histórico de alterações do DB
+
+- Sincroniza os banco de dados
+
+### Criar tabela de usuários
+
+(anotar passos da migração)
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,11 @@
 import "reflect-metadata";
 import express from "express";
 import "./database"; //omitimos o nome index.ts por ser esse o que essa conexão busca primeiro
+import { router } from "./routes"; //45:42 video.
 
 const app = express();
 
-/**
- * GET => Busca
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Modificar
- * PATCH => Alteração Específica
- **/
-
-// http://localhost:3333/users
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World - JSON - NLW04" });
-});
-
-app.post("/", (request, response) => {
-  return response.json({ message: "Os dados foram salvos com sucesso!" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!!"));
